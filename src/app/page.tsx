f4216d0b9a355c620794ab8e43b228756987780e
priyansudash1999 <1999priyansudash@gmail.com>
2026-03-22 15:11:48 +0530

    feat(db): integrate Neon database with Drizzle ORM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 const page = () => {
   return (
     <div className='text-4xl'>
-      Home Page
+      <Button variant="custom">
+        Click
+      </Button>
     </div>
   )
 }
